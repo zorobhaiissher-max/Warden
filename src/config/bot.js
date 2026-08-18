19abb92ef45e4352e130364186d567f72fc910ed
zorobhaiissher-max <zorobhaiissher@gmail.com>
2026-08-18 22:10:48 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Join Dsc.gg/wardenlands",     // this is what people actually see
+        state: "ᴊᴏɪɴ ᴅѕᴄ.ɢɢ/ᴡᴀʀᴅᴇɴʟᴀɴᴅѕ",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
