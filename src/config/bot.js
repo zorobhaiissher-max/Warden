8b41be6056d1845d9aae640ab8b945315a581723
zorobhaiissher-max <zorobhaiissher@gmail.com>
2026-08-18 23:31:33 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "ᴊᴏɪɴ ᴅѕᴄ.ɢɢ/ᴡᴀʀᴅᴇɴʟᴀɴᴅѕ",     // this is what people actually see
+        state: "ᴊᴏɪɴ ᴅѕᴄ.ɢɢ/ᴡᴀʀᴅɪᴜᴍ",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
